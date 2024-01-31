e1b573263ea1cf4412c64d314f12966f7bf87e4a
qgene79 <103479839+qgene79@users.noreply.github.com>
2024-01-31 14:02:58 +0900

    draw triangle

--- src/context.cpp
+++ src/context.cpp
@@ -11,10 +11,39 @@ void Context::Render() {
     glClear(GL_COLOR_BUFFER_BIT);
 
     glUseProgram(m_program->Get());
-    glDrawArrays(GL_POINTS, 0, 1);
+    glDrawArrays(GL_TRIANGLES, 0, 3);  
+    //glDrawArrays(GL_LINE_STRIP, 0, 4);
 }
 
 bool Context::Init() {
+
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f, 0.5f, 0.0f,
+    //    -0.5f, -0.5f, 0.0f,
+    };
+
+    //vertex array object(vao) 생성 (vertex 버퍼 전에 선언)
+    glGenVertexArrays(1, &m_vertexArrayObject);
+    glBindVertexArray(m_vertexArrayObject);
+
+    //vertex buffer object(vbo) 생성
+    glGenBuffers(1, &m_vertexBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, m_vertexBuffer);//GL_ARRAY_BUFFER == vbo vertex의 속성값 색 위치 등
+    //data 복사
+    //GL_STATIC_DRAW 딱 한번만 세팅되고 앞으로 계속 쓸 예정
+    //GL_DYNAMIC_DRAW 앞으로 데이터가 자주 바뀔 예정
+    //GL_STREAM_DRAW 딱 한번만 세팅되고 몇번 쓰다 버려질 예정
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 9, vertices, GL_STATIC_DRAW);
+    //glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 12, vertices, GL_STATIC_DRAW);
+
+    //vertex attribute setting
+    glEnableVertexAttribArray(0);// 0번 어트리뷰트를 사용 할꺼고 (simple.vs 의 location = 0)
+    //floating pointer 값이 3개, GL_FALSE 노말라이즈 할필요 없음, sizeof(float) * 3 스트라이드: 다음 점에서의 어트르뷰트는 float * 3 개 
+    //결국 위 어레이 값을 0번째 부터 3개씩 float 값으로 쪼개 simple.vs 에 aPos vec3(vector 3 형태로)에 넣음
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, 0);
+
     ShaderPtr vertShader = Shader::CreateFromFile("./shader/simple.vs", GL_VERTEX_SHADER);
     ShaderPtr fragShader = Shader::CreateFromFile("./shader/simple.fs", GL_FRAGMENT_SHADER);
     if (!vertShader || !fragShader)
@@ -29,10 +58,6 @@ bool Context::Init() {
 
     //전체 Clear 할 색 지정
     glClearColor(0.0f, 0.1f, 0.2f, 0.0f);
-
-    uint32_t vao = 0;
-    glGenVertexArrays(1, &vao);
-    glBindVertexArray(vao);
 
     return true;
 }
