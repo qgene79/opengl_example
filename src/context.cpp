7cf5ecf347f68413e505f2dbe8c36b37eb9e7aed
qgene79 <103479839+qgene79@users.noreply.github.com>
2024-02-01 17:26:59 +0900

    vertex, fragment shader 추가 그라데이션 삼각형

--- src/context.cpp
+++ src/context.cpp
@@ -10,18 +10,23 @@ ContextUPtr Context::Create() {
 void Context::Render() {
     glClear(GL_COLOR_BUFFER_BIT);
 
+    // static float time = 0.0f;
+    // float t = sinf(time) * 0.5f + 0.5f;
+    // auto loc = glGetUniformLocation(m_program->Get(), "color");
     m_program->Use();
+    // glUniform4f(loc, t*t, 2.0f*t*(1.0f-t), (1.0f-t)*(1.0f-t), 1.0f);
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-    //glDrawArrays(GL_LINE_STRIP, 0, 4);
+
+    // time += 0.0016f;
 }
 
 bool Context::Init() {
 
     float vertices[] = {
-        0.5f, 0.5f, 0.0f, // top right 0 
-        0.5f, -0.5f, 0.0f, // bottom right 1
-        -0.5f, -0.5f, 0.0f, // bottom left 2
-        -0.5f, 0.5f, 0.0f, // top left 3
+        0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // top right, red
+        0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // bottom right, green
+        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, // bottom left, blue
+        -0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f, // top left, yellow
         };
     uint32_t indices[] = { // note that we start from 0!
         0, 1, 3, // first triangle
@@ -32,28 +37,37 @@ bool Context::Init() {
     m_vertexLayout = VertexLayout::Create();
 
     //vertex buffer object(vbo) 생성
-    m_vertexBuffer = Buffer::CreateWithData(GL_ARRAY_BUFFER, GL_STATIC_DRAW, vertices, sizeof(float) * 12);
+    m_vertexBuffer = Buffer::CreateWithData(GL_ARRAY_BUFFER, GL_STATIC_DRAW, vertices, sizeof(float) * 24);
 
     //vertex attribute array setting
     //floating pointer 값이 3개, GL_FALSE 노말라이즈 할필요 없음, sizeof(float) * 3 스트라이드: 다음 점에서의 어트르뷰트는 float * 3 개 
     //0번 어트리뷰트를 사용 할꺼고 (simple.vs 의 location = 0)
     //결국 위 어레이 값을 0번째 부터 3개씩 float 값으로 쪼개 simple.vs 에 aPos vec3(vector 3 형태로)에 넣음
-    m_vertexLayout->SetAttrib(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, 0);
+    //m_vertexLayout->SetAttrib(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, 0);
+    m_vertexLayout->SetAttrib(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, 0);
+    m_vertexLayout->SetAttrib(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, sizeof(float) * 3);
 
     //index buffer
     m_indexBuffer = Buffer::CreateWithData(GL_ELEMENT_ARRAY_BUFFER, GL_STATIC_DRAW, indices, sizeof(uint32_t) * 6);
 
-    ShaderPtr vertShader = Shader::CreateFromFile("./shader/simple.vs", GL_VERTEX_SHADER);
-    ShaderPtr fragShader = Shader::CreateFromFile("./shader/simple.fs", GL_FRAGMENT_SHADER);
+    //loading shader
+    ShaderPtr vertShader = Shader::CreateFromFile("./shader/per_vertex_color.vs", GL_VERTEX_SHADER);
+    ShaderPtr fragShader = Shader::CreateFromFile("./shader/per_vertex_color.fs", GL_FRAGMENT_SHADER);
     if (!vertShader || !fragShader)
         return false;
     SPDLOG_INFO("vertex shader id: {}", vertShader->Get());
     SPDLOG_INFO("fragment shader id: {}", fragShader->Get());
 
+    //program 인스턴스 생성, shader를 링크한 프로그램 생성
     m_program = Program::Create({fragShader, vertShader});
     if (!m_program)
         return false;
     SPDLOG_INFO("program id: {}", m_program->Get());
+
+    //uniform 값 입력 추가 
+    // auto loc = glGetUniformLocation(m_program->Get(), "color");//fs의 color
+    // m_program->Use();
+    // glUniform4f(loc, 1.0f, 1.0f, 0.0f, 1.0f);
 
     //전체 Clear 할 색 지정
     glClearColor(0.0f, 0.1f, 0.2f, 0.0f);
