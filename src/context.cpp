9fa2e5006cea44c611d2863f690ded026a66f669
qgene79 <103479839+qgene79@users.noreply.github.com>
2024-02-02 16:22:06 +0900

    tecture refactoring

--- src/context.cpp
+++ src/context.cpp
@@ -86,16 +86,7 @@ bool Context::Init() {
         image->GetWidth(), image->GetHeight(), image->GetChannelCount());
 
     //gpu memory 할당
-    glGenTextures(1, &m_texture);
-    glBindTexture(GL_TEXTURE_2D, m_texture);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 
-        image->GetWidth(), image->GetHeight(), 0, 
-        GL_RGB, GL_UNSIGNED_BYTE, image->GetData());
-
+    m_texture = Texture::CreateFromImage(image.get());
+    
     return true;
 }
