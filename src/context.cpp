f49584348dc1ed039f51fcb1e80595172e40ca14
qgene <qgene79@gmail.com>
2024-02-22 20:26:31 +0900

    material map - 2

--- src/context.cpp
+++ src/context.cpp
@@ -33,7 +33,6 @@ void Context::Render() {
         }
  
         if (ImGui::CollapsingHeader("material", ImGuiTreeNodeFlags_DefaultOpen)) {
-            ImGui::ColorEdit3("m.specular", glm::value_ptr(m_material.specular));
             ImGui::DragFloat("m.shininess", &m_material.shininess, 1.0f, 1.0f, 256.0f);
         }
         ImGui::Checkbox("animation", &m_animation);
@@ -89,11 +88,13 @@ void Context::Render() {
     m_program->SetUniform("light.diffuse", m_light.diffuse);
     m_program->SetUniform("light.specular", m_light.specular);
     m_program->SetUniform("material.diffuse", 0);
-    m_program->SetUniform("material.specular", m_material.specular);
+    m_program->SetUniform("material.specular", 1);
     m_program->SetUniform("material.shininess", m_material.shininess);
 
 	glActiveTexture(GL_TEXTURE0);
     m_material.diffuse->Bind();
+    glActiveTexture(GL_TEXTURE1);
+    m_material.specular->Bind();
 
     for (size_t i = 0; i < cubePositions.size(); i++) {
         auto& pos = cubePositions[i];
@@ -269,11 +270,14 @@ bool Context::Init() {
     }
     m_texture2 = Texture::CreateFromImage(image2.get());
 
-    auto imageC = Image::Load("./images/container.jpg");
+    auto imageC = Image::Load("./images/container2.png");
     if (!imageC) {
         return false;
     }
     m_material.diffuse = Texture::CreateFromImage(imageC.get());
+
+    m_material.specular = Texture::CreateFromImage(
+        Image::Load("./images/container2_specular.png").get());
     
     glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, m_texture->Get());
